308e82dd333c074ea0fc481a35499124d7947791
JeffreyMcPartlan <42681408+JeffreyMcPartlan@users.noreply.github.com>
2018-08-24 15:15:50 -0400

    Add files via upload

---
+++ BuzzFizz/ClassLibrary1/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class BuzzFizz
+    {
+        private int upperBound;
+        public BuzzFizz(int intUpperBound)
+        {
+            this.upperBound = intUpperBound;
+        }
+
+        public static void StaticOutput(int upperBound)
+        {
+            
+            for (int i = 1; i <= upperBound; i++)
+            {
+                string strReturn = "";
+                if (i % 3 == 0)
+                {
+                    strReturn += "Buzz";
+                }
+                if (i % 5 == 0)
+                {
+                    strReturn += "Fizz";
+                }
+                if ((i % 3 != 0) && (i % 5 != 0))
+                {
+                    strReturn += i.ToString();
+                }
+                Console.WriteLine(strReturn);
+            }
+        }
+        public void ObjectInstanceOutput()
+        {
+            for (int i = 1; i <= this.upperBound; i++)
+            {
+                string strReturn = "";
+                if (i % 3 == 0)
+                {
+                    strReturn += "Buzz";
+                }
+                if (i % 5 == 0)
+                {
+                    strReturn += "Fizz";
+                }
+                if ((i % 3 != 0) && (i % 5 != 0))
+                {
+                    strReturn += i.ToString();
+                }
+                Console.WriteLine(strReturn);
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This will write to the Cosnole
+            Console.WriteLine("This will output BuzzFizz using a static class method to the UpperBound of 100.");
+            BuzzFizz.StaticOutput(100);
+            Console.ReadKey();
+            Console.WriteLine();
+
+            //This will write to the Cosnole
+            Console.WriteLine("This will output BuzzFizz using an instantiated object of BuzzFizz \n " +
+                "to the UpperBound of 150 which is set when the object is constructed.");
+            BuzzFizz BF = new BuzzFizz(150);
+            BF.ObjectInstanceOutput();
+            Console.ReadKey();
+            Console.WriteLine();
+
+            //If you wanted to run this Console Application from the command line 
+            //by passing an integer value as the first parameter
+            Console.WriteLine("This will output BuzzFizz using a static class mehod to the UpperBound of \n " +
+                "the int value passed into the console application as the first parameter.");
+            int parameter;
+            if (args.Count() > 0)
+            { 
+                if (int.TryParse(args[0], out parameter))
+                {
+                    BuzzFizz.StaticOutput(Convert.ToInt32(args[0]));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Argument: " + args[0]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Argugment passed into the application.");
+            }
+        }
+    }
+}
