2d5b19a6c3248c494fb798e7a0c650f1ae71f7b6
JeffreyMcPartlan <42681408+JeffreyMcPartlan@users.noreply.github.com>
2018-08-25 00:03:51 -0400

    Add files via upload
    Adding a PowerShell script which loads the ClassLibrary1.dll and then uses it to execute static methods and create object instances.

---
+++ PSScripts/BuzzFizzPSScript.ps1
@@ -0,0 +1,11 @@
+﻿Write-Output "`nLoad our DLL.`n"
+[System.Reflection.Assembly]::LoadFrom("C:\Users\jeffrey.mcpartlan\Documents\Visual Studio 2017\Projects\BuzzFizz\ClassLibrary1\bin\Release\ClassLibrary1.dll")
+
+Write-Output "`nExecute the static method of our class.`n"
+[ClassLibrary1.BuzzFizz]::StaticOutput(350);
+
+Write-Output "`nCreate an object instance of our class.`n"
+$MyCompObj = New-Object ClassLibrary1.BuzzFizz(100);
+
+Write-Output "`nExecute the public method of our instantiated class.`n"
+$MyCompObj.ObjectInstanceOutput();
